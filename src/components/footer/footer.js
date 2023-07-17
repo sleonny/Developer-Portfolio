377ecdb55d705562a9ecad55ed6a1bb3eb6ff35c
Sean L <leonardmsean@gmail.com>
2023-07-17 17:41:48 -0400

    finished footer hrefs

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,13 +1,37 @@
 import React from "react";
 import "./Footer.css";
 
-const Footer = () {
+const Footer = () => {
   return (
     <footer className="footer">
-    <a href="https://github.com/sleonny" target="_blank" rel="noreferrer">
-    <img src={process.env.PUBLIC_URL + '/public/githublogo.png'} alt="Github" />
-    </a>
-    
+      <a href="https://github.com/sleonny" target="_blank" rel="noreferrer">
+        <img
+          src={process.env.PUBLIC_URL + "/public/githublogo.png"}
+          alt="Github"
+        />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/sean-leonard-23280010b/"
+        target="_blank"
+        rel="roreferrer"
+      >
+        <img
+          src={process.env.PUBLIC_URL + "/public/linkedinlogo.png"}
+          alt="LinkedIn"
+        />
+      </a>
+      <a
+        href="https://www.facebook.com/sean.leonard.359126/"
+        target="blank"
+        rel="noreferrer"
+      >
+        <img
+          src={process.env.PUBLIC_URL + "/public/facebooklogo.png"}
+          alt="Facebook"
+        />
+      </a>
     </footer>
-  )
-}
+  );
+};
+
+export default Footer;
