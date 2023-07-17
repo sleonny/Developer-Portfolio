4ffc07d1038cfd8c5b9e7de777ad2ef49ba34a11
Sean L <leonardmsean@gmail.com>
2023-07-17 17:36:20 -0400

    added logos for facebook, github and linkedin to public folder

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,33 +1,13 @@
 import React from "react";
 import "./Footer.css";
 
-const Footer = () => {
+const Footer = () {
   return (
     <footer className="footer">
-      <a
-        href="https://github.com/YourGitHubUsername"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        GitHub
-      </a>
-      <a
-        href="https://linkedin.com/in/YourLinkedInUsername"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        LinkedIn
-      </a>
-      <a
-        href="https://facebook.com/YourFacebookUsername"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Facebook
-      </a>
-      {/* Add more links as you need */}
+    <a href="https://github.com/sleonny" target="_blank" rel="noreferrer">
+    <img src={process.env.PUBLIC_URL + '/public/githublogo.png'} alt="Github" />
+    </a>
+    
     </footer>
-  );
-};
-
-export default Footer;
+  )
+}
