e7c5c3311626afb2b58e1cf5d14c445f0b10c770
Sean L <leonardmsean@gmail.com>
2023-07-17 15:00:53 -0400

    added aboutme code

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import AboutMe from "./components/AboutMe/AboutMe";
--- src/components/aboutme/aboutme.js
+++ src/components/aboutme/aboutme.js
@@ -1,11 +1,30 @@
-import React from "react";
-import "./AboutMe.css";
+import React, { useState } from "react";
+import MapComponent from "./MapComponent"; // Import your map component
 
 const AboutMe = () => {
+  const [isMapVisible, setIsMapVisible] = useState(true);
+
   return (
     <div className="about-me">
-      <img src="avatar.png" alt="Your Avatar" />
-      <p>Hi, I'm John Doe, a Full Stack Web Developer. I specialize in...</p>
+      <div className="intro">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
+          purus quis vehicula hendrerit.
+        </p>
+        <p>
+          Mauris ultrices purus non turpis sagittis, a posuere lectus ornare.
+          Integer tincidunt nunc sit amet elit tincidunt, in pharetra sem
+          fermentum.
+        </p>
+        <p>
+          Fusce sodales odio a augue dictum, nec sagittis massa gravida. Vivamus
+          volutpat nisl quis libero efficitur, at sollicitudin nulla luctus.
+        </p>
+      </div>
+      <h3 className="map-title">
+        Explore the places I've been by hovering over the cities on the map!
+      </h3>
+      {isMapVisible && <MapComponent />}
     </div>
   );
 };
