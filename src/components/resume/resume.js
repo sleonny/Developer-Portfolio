20f015f969db39741045e4205ef229dbc1f85ffe
Sean L <leonardmsean@gmail.com>
2023-07-19 14:03:59 -0400

    filled in resume details

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -10,54 +10,128 @@ const Resume = () => {
       <div className="resume-section">
         <h2>Experience</h2>
         <div className="job" onClick={() => setSelectedJob("Job1")}>
-          <h3>Title 1</h3>
+          <h3>General Manager - Buffalo Wild Wings - Leesburg, VA</h3>
         </div>
-        {selectedJob === "Job1" && <p>Job Description</p>}
+        {selectedJob === "Job1" && (
+          <p>
+            Instrumental in managing the comprehensive operations of a bustling
+            chicken wing restaurant and sports bar, with a key focus on
+            providing superior food quality, exceptional customer service, and
+            engaging sports entertainment. Developed and executed innovative
+            marketing strategies that significantly enhanced brand visibility
+            and customer footfall. Orchestrated recruitment, training, and
+            management of a highly effective team, fostering a supportive work
+            environment that encouraged growth and development. Demonstrated
+            proficiency in menu planning, supply chain oversight, budgeting, and
+            financial planning to optimize business profitability. Implemented
+            stringent hygiene standards and safety procedures, ensuring
+            compliance with local and federal regulations. Successfully enhanced
+            customer relations through direct engagement and feedback
+            resolution, resulting in an increased repeat customer rate.
+            Regularly analyzed performance metrics, driving continuous
+            improvement and business growth in line with corporate policies and
+            objectives.
+          </p>
+        )}
 
         <div className="job" onClick={() => setSelectedJob("Job2")}>
-          <h3>Title 2</h3>
+          <h3>Director of Food & Beverage - River Creek Club - Leesburg, VA</h3>
         </div>
-        {selectedJob === "Job2" && <p>Job Description</p>}
+        {selectedJob === "Job2" && (
+          <p>
+            Championed as the key executive overseeing the food and beverage
+            operations at a premium country club, managing a full-service
+            restaurant renowned for its exquisite wine list, private events
+            including weddings and parties, and catering for golf tournaments.
+            Showcased excellence in curating and maintaining an extensive wine
+            list that enhanced the fine dining experience and increased revenue.
+            Supervised the planning and execution of numerous private events,
+            ensuring all services met and exceeded members' expectations.
+            Orchestrated seamless catering services for large-scale golf
+            tournaments, reinforcing the club's reputation for superior service.
+            Demonstrated adeptness in personnel management, from recruitment to
+            training, fostering a culture of excellence and commitment. Directed
+            budgeting, financial planning, and inventory management, ensuring
+            cost-effective operations without compromising on quality.
+            Skillfully negotiated with suppliers and vendors for top-quality
+            ingredients and beverages. Employed a member-centric approach to
+            improve relations, solicit feedback, and implement changes to
+            deliver unparalleled dining and event experiences. Consistently
+            monitored performance metrics, driving continuous improvement and
+            aligning the food and beverage department's goals with the club's
+            vision and objectives.
+          </p>
+        )}
 
         <div className="job" onClick={() => setSelectedJob("Job3")}>
-          <h3>Title 3</h3>
+          <h3>
+            Office Administrator/Assistant Accountant - River Creek Club -
+            Leesburg, VA
+          </h3>
         </div>
-        {selectedJob === "Job3" && <p>Job Description</p>}
+        {selectedJob === "Job3" && (
+          <p>
+            Serving in a dual role as Assistant Accountant and Office Manager,
+            proficiently handling a broad range of financial and administrative
+            duties. Entrusted with managing accounts payable and receivable,
+            executing precise and timely billing, and addressing and resolving
+            any billing disputes. Demonstrated expertise in processing payments,
+            managing cash flows, and conducting periodic account
+            reconciliations, ensuring the highest level of financial accuracy.
+            Played an integral role in document management, including
+            organizing, filing, and archiving important financial and
+            administrative records, which greatly increased operational
+            efficiency. Developed and implemented robust financial procedures
+            and controls that significantly improved the company's financial
+            health. Proactively participated in financial reporting, providing
+            key insights that informed strategic decision-making. Beyond
+            financial responsibilities, adeptly managed the smooth functioning
+            of the office, coordinating with different departments, managing
+            schedules, and handling office supplies. Exhibited exceptional
+            interpersonal skills, liaising effectively with clients, suppliers,
+            and staff, further solidifying relationships and ensuring the office
+            environment remains productive and professional.
+          </p>
+        )}
 
         <div className="resume-section">
           <h2>Education</h2>
           <div className="school" onClick={() => setSelectedSchool("School1")}>
-            <h3>School Name 1</h3>
+            <h3>George Washington University</h3>
           </div>
-          {selectedSchool === "School1" && <p>Degree</p>}
+          {selectedSchool === "School1" && (
+            <p>Full Stack Web Development Certification</p>
+          )}
 
           <div className="school" onClick={() => setSelectedSchool("School2")}>
-            <h3>School Name 2</h3>
+            <h3>Northern Virginia Community College</h3>
           </div>
-          {selectedSchool === "School2" && <p>Degree</p>}
+          {selectedSchool === "School2" && (
+            <p>Some progress towards Associates</p>
+          )}
 
           <div className="school" onClick={() => setSelectedSchool("School3")}>
-            <h3>School Name 3</h3>
+            <h3>Loudoun Valley High School</h3>
           </div>
-          {selectedSchool === "School3" && <p>Degree</p>}
+          {selectedSchool === "School3" && <p>Advanced Diploma</p>}
         </div>
         <div className="resume-section">
           <h2>Skills</h2>
           <ul className="skills-list">
-            <li>Skill 1</li>
-            <li>Skill 2</li>
-            <li>Skill 3</li>
-            <li>Skill 4</li>
-            <li>Skill 5</li>
-            <li>Skill 6</li>
-            <li>Skill 7</li>
-            <li>Skill 8</li>
-            <li>Skill 9</li>
-            <li>Skill 10</li>
-            <li>Skill 11</li>
-            <li>Skill 12</li>
-            <li>Skill 13</li>
-            <li>Skill 14</li>
+            <li>HTML, CSS</li>
+            <li>Javascript</li>
+            <li>Web, 3rd Party & Server-side API's</li>
+            <li>node.js</li>
+            <li>Object-Oriented Programming</li>
+            <li>express.js</li>
+            <li>SQL</li>
+            <li>Object-Relational Mapping</li>
+            <li>Model-View-Controller</li>
+            <li>NoSQL - MongoDB & Mongoose</li>
+            <li>Progressive Web Applications</li>
+            <li>React</li>
+            <li>State</li>
+            <li>MERN Full Stack</li>
           </ul>
         </div>
 
